eed9d4323ddd9d9fd7be93146ece23da41562c8a
Wellison Cavalcante <wellisoncavalcante46@gmail.com>
2026-03-03 17:13:47 -0300

    -

--- src/teste/AgendaTeste01.java
+++ src/teste/AgendaTeste01.java
@@ -5,7 +5,6 @@ import dominio.Contato;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class AgendaTeste01 {
